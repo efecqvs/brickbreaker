deed0acd632b6413695b0e0582c781bed4052823
efecqvs <efecavus0308@gmail.com>
2026-05-04 19:43:17 +0300

    Add files via upload

--- src/systems/InputManager.js
+++ src/systems/InputManager.js
@@ -10,31 +10,51 @@ export default class InputManager {
     }
 
     init() {
-        this.canvas.addEventListener("mousedown", (e) => { 
+        const handleStart = (e) => { 
             e.preventDefault();
             if(this.game.gameState === "PLAYING" && !this.game.isFiring) { 
                 this.isAiming = true; 
                 this.updateAim(e); 
             }
-        });
-        
-        this.canvas.addEventListener("mousemove", (e) => { 
+        };
+
+        const handleMove = (e) => { 
             e.preventDefault();
             if(this.isAiming) this.updateAim(e); 
-        });
-        
-        this.canvas.addEventListener("mouseup", (e) => { 
+        };
+
+        const handleEnd = (e) => { 
             e.preventDefault();
             if(this.isAiming) { 
                 this.isAiming = false; 
                 this.game.startFire(); 
             }
-        });
+        };
+
+        this.canvas.addEventListener("mousedown", handleStart);
+        this.canvas.addEventListener("touchstart", handleStart, {passive: false});
+        
+        this.canvas.addEventListener("mousemove", handleMove);
+        this.canvas.addEventListener("touchmove", handleMove, {passive: false});
+        
+        this.canvas.addEventListener("mouseup", handleEnd);
+        this.canvas.addEventListener("touchend", handleEnd, {passive: false});
     }
 
     updateAim(e) {
         const rect = this.canvas.getBoundingClientRect();
-        this.targetX = e.clientX - rect.left;
-        this.targetY = e.clientY - rect.top;
+        const scaleX = this.canvas.width / rect.width;
+        const scaleY = this.canvas.height / rect.height;
+        
+        let clientX = e.clientX;
+        let clientY = e.clientY;
+        
+        if(e.touches && e.touches.length > 0) {
+            clientX = e.touches[0].clientX;
+            clientY = e.touches[0].clientY;
+        }
+
+        this.targetX = (clientX - rect.left) * scaleX;
+        this.targetY = (clientY - rect.top) * scaleY;
     }
 }
